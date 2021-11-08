7db31ad03a7cb1848dd32ab608a5be0a6acd697f
emel <emel@DESKTOP-4DK400G>
2021-11-08 23:28:44 +0900

    1. mainpage.java -> Console.java 통합 2. PostPage.java -> Console.java 통합 및 함수명 변경   postView -> printPost   selectWork_ver_non -> printPostSelection_guest   selectWork_ver_trav -> printPostSelection_traveler   selectWork_ver_admin - > printPostSelection_admin   selectWork_ver_writter ->  printPostSelection_traveler_writer 3. 메뉴 표시 분할(printPostTable, printMainMenu{printMainMenu_guest, printMainMenu_traveler}) 4. 페이지 표시 추가, 첫/마지막 페이지 확인 구현 5. int[] postNum = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, int count = 0 -> printMainMenu > 선택 mode > sql 실행으로 대체

--- LanTravel/src/LanTravel/main.java
+++ LanTravel/src/LanTravel/main.java
@@ -5,17 +5,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class main {
-	public static final String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
-	public static final String USER_NAME ="project";	// tb??
-	public static final String USER_PASSWD ="comp322";
-	
 	public static void main(String[] args) {
 		Console cs = new Console();
-		
+
 		cs.printMainMenu();
-		
-		
-//		mainpage mp = new mainpage();
-//		mp.mainpage(conn, stmt);
 	}
 }
