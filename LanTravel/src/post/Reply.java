705025d9bedd266515cd9b0065611d91be4d0a53
jhdong008 <jhdong008@naver.com>
2021-12-01 18:38:12 +0900

    reply 관련 java 파일 생성 (진행 중)

--- LanTravel/src/post/Reply.java
+++ LanTravel/src/post/Reply.java
@@ -1,5 +1,73 @@
 package post;
 
-public class Reply {
+import java.sql.Date;
 
+public class Reply {
+	private int num;
+	private String text;
+	private Date writtenTime;
+	private int travelerNum;
+	private int parentNum;
+	private int postNum;
+
+	public Reply() {
+		
+	}
+
+	public Reply(int num, String text, Date writtenTime, int travelerNum, int parentNum) {
+		super();
+		this.num = num;
+		this.text = text;
+		this.writtenTime = writtenTime;
+		this.travelerNum = travelerNum;
+		this.parentNum = parentNum;
+	}
+	
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getWrittenTime() {
+		return writtenTime;
+	}
+
+	public void setWrittenTime(Date writtenTime) {
+		this.writtenTime = writtenTime;
+	}
+
+	public int getTravelerNum() {
+		return travelerNum;
+	}
+
+	public void setTravelerNum(int travelerNum) {
+		this.travelerNum = travelerNum;
+	}
+
+	public int getParentNum() {
+		return parentNum;
+	}
+
+	public void setParentNum(int parentNum) {
+		this.parentNum = parentNum;
+	}
+
+	public int getPostNum() {
+		return postNum;
+	}
+
+	public void setPostNum(int postNum) {
+		this.postNum = postNum;
+	}
 }
--- LanTravel/src/post/ReplyDAO.java
+++ LanTravel/src/post/ReplyDAO.java
@@ -1,5 +1,84 @@
 package post;
 
-public class ReplyDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+public class ReplyDAO {
+	private Connection conn = null;
+	private Statement stmt = null;
+	private ResultSet rs;
+
+	public static final String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+	public static final String USER_NAME = "project";
+	public static final String USER_PASSWD = "comp322";
+
+	public ReplyDAO() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("Driver loading : Success!");
+		} catch (ClassNotFoundException e) {
+			System.err.println("error = " + e.getMessage());
+			System.exit(1);
+		}
+
+		try {
+			conn = DriverManager.getConnection(URL, USER_NAME, USER_PASSWD);
+			System.out.println("Oracle Connected.");
+			stmt = conn.createStatement();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.err.println("Cannot get a connection: " + ex.getLocalizedMessage());
+			System.err.println("Cannot get a connection: " + ex.getMessage());
+			System.exit(1);
+		}
+	}
+	
+	public String getCurrTime() {
+		String sql = "SELECT SYSDATE FROM DUAL";
+		try {
+			rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+				return rs.getString(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null; // DB 오류
+	}
+	
+	public int getNextNum() {
+		String sql = "SELECT NVL(MAX(reply_num), 0) FROM reply";
+		try {
+			rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+				return rs.getInt(1) + 1;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // DB 오류
+	}
+	
+	public ArrayList<Reply> getList(int postNum) {
+		String sql = "SELECT reply_num, text, written_time, traveler_num, p_reply_num FROM reply WHERE post_num = ? ORDER BY reply_num DESC";
+		ArrayList<Reply> list = new ArrayList<Reply>();
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, postNum);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				// reply_num, text, written_time, traveler_num, p_reply_num
+				Reply reply = new Reply(rs.getInt(1), rs.getString(2), rs.getDate(3), rs.getInt(4), rs.getInt(5));
+				list.add(reply);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 }
